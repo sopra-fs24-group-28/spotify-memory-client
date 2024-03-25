6ef987805d5ac1768a3b71625d9790241dc8cf7a
Niklas Schmidt <niklas.schmidt@uzh.ch>
2024-03-25 19:12:40 +0100

    created navbar as general feature comforming to issue number #37

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import Header from "./components/views/Header";
 import AppRouter from "./components/routing/routers/AppRouter";
+import Navbar from "./components/ui/Navbar";
 
-/**
- * Happy coding!
- * React Template by Lucas Pelloni
- * Overhauled by Kyrill Hux
- * Updated by Marco Leder
- */
 const App = () => {
   return (
     <div>
-      <Header height="100" />
+      <Navbar height="100" />
       <AppRouter />
     </div>
   );
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "../../styles/ui/Navbar.scss";
-import GameIconSVG from "../../styles/iconpaths/GameIconSVG";
-import PreferencesSVG from "../../styles/iconpaths/PreferencesSVG";
-import MuteSVG from "../../styles/iconpaths/MuteSVG";
-import LogoutSVG from "../../styles/iconpaths/LogoutSVG";
-import HomeSVG from "../../styles/iconpaths/HomeSVG";
+import GameIconSVG from "./icons-svg/GameIconSVG";
+import LogoutSVG from "./icons-svg/LogoutSVG";
+import MuteSVG from "./icons-svg/MuteSVG";
+import PreferencesSVG from "./icons-svg/PreferencesSVG";
+
 
 
 const Navbar = () => {
   return (<nav className="navbar">
     <div className="navbar-container">
-      <a href="/" className="navbar-brand">
+      <span href="/" className="navbar-brand">
         Spotymemory
-      </a>
+      </span>
       <div className="navbar-icons">
 
         <a href="/lobbyOverview"  className="iconItems">
