320238eef676e20782df06b81fda9a2230088f92
Diyar Taskiran <diyar.taskiran@uzh.ch>
2024-03-28 20:24:18 +0100

    Added api call to logout, complements PR #40 by completing dev task #31

--- src/helpers/auth/logoutfunction.js
+++ src/helpers/auth/logoutfunction.js
@@ -1,4 +1,8 @@
-export const logout = (navigate) => {
+import { api } from "../api";
+
+export const logout = async (navigate) => {
+ 
+  const response = await api.delete("/auth/token")
   localStorage.clear()
   console.log("User logged out");
   navigate('/login');
