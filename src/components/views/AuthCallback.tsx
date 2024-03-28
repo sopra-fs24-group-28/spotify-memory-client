e39298bdb4ff736d0feabd1210660c59a02ea622
Niklas Schmidt <niklas.schmidt@uzh.ch>
2024-03-28 18:53:52 +0100

    refactoring for diyar, added ternary operators here and there to prevent errors, adjusted minor bug in Button.tsx #35

--- src/components/views/AuthCallback.tsx
+++ src/components/views/AuthCallback.tsx
@@ -1,86 +1,69 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { api, handleError } from "helpers/api";
-import User from "models/User";
-import {useNavigate, useSearchParams} from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { Button } from "components/ui/Button";
 import "styles/views/Login.scss";
 import BaseContainer from "components/ui/BaseContainer";
-import PropTypes from "prop-types";
 
 const AuthCallback = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const [responseMessage, setResponseMessage] = useState();
-  const code = searchParams.get('code');
-  const state = searchParams.get('state');
-  
+  const code = searchParams.get("code");
 
   useEffect(() => {
     async function auth_user() {
       try {
         const requestBody = JSON.stringify({ code });
-        const response = await api.post('/auth/token', requestBody);
-        
-        // Store the token into the local storage.
+        console.log("successfully fetched token");
+        const response = await api.post("/auth/token", requestBody);
         // assuming response already 200, otherwise would have been caught by handleError
         localStorage.setItem("token", response.data);
         // routing user to lobby overview screen upon successful login
         navigate("/overview");
-        
-        // Login successfully worked --> navigate to the route /game in the GameRouter
+
       } catch (error) {
         console.log(error);
-        setResponseMessage(error.message + " – " + error.response.data.message);
-        // alert(
-        //   `Something went wrong during the login: \n${handleError(error)}`
-        // );
-        // navigate("/login");
+        setResponseMessage(`${error.message ?? "There was as error, please try again"} ${error.response?.data?.message ?? ""}`);
+        console.log(responseMessage);
       }
     }
+
     auth_user();
-    }, [] );
+  }, []);
 
   const returnToLogin = () => {
     try {
       navigate("/login");
     } catch (error) {
-      alert(
-        `Something went wrong, could not return to login: \n${handleError(error)}`
-      );
+      alert(`Something went wrong, could not return to login: \n${handleError(error)}`);
     }
   };
 
-  let body = "Redirecting ..."
+  let body = "Redirecting ...";
 
   if (responseMessage) {
-    console.log(responseMessage)
-    body = (
-      <div>
-        <div>{responseMessage}</div>
-        <div className="login button-container">
-          <Button width="20em" 
-                  onClick={() => returnToLogin()}>Return to Login</Button>
-        </div>
+    console.log(responseMessage);
+    body = (<div>
+      <div>{responseMessage}</div>
+      <div className="login button-container">
+        <Button width="20em"
+                onClick={() => returnToLogin()}>Return to Login</Button>
       </div>
-    );
-    
+    </div>);
+
 
   }
-
-
-  return ( <BaseContainer>
-      <div className="login container">
-        <div className="login form">
-          <div className="login button-container">
-              {body}
-          </div>
+  return (<BaseContainer>
+    <div className="login container">
+      <div className="login form">
+        <div className="login button-container">
+          {body}
         </div>
       </div>
-    </BaseContainer>
-  );
+    </div>
+  </BaseContainer>);
 };
 
-/**
- * You can get access to the history object's properties via the useLocation, useNavigate, useParams, ... hooks.
- */
+
 export default AuthCallback;
--- src/components/views/Login.tsx
+++ src/components/views/Login.tsx
@@ -1,63 +1,39 @@
-import React, { useState } from "react";
-import axios from "axios";
-import User from "models/User";
-import {useNavigate} from "react-router-dom";
+import React from "react";
 import { Button } from "components/ui/Button";
 import "styles/views/Login.scss";
 import BaseContainer from "components/ui/BaseContainer";
-import PropTypes from "prop-types";
 
 const Login = () => {
-  const navigate = useNavigate();
-  
-
   const doLogin = async () => {
     try {
-      const client_id = "5aac3ff5093942be92372c19a12fdecd";
-      const scope = "streaming playlist-read-private playlist-read-collaborative";
-      const redirect_uri = "http://localhost:3000/auth_callback";
-      const urlBase = "https://accounts.spotify.com/authorize?";
+      const client_id: string = "5aac3ff5093942be92372c19a12fdecd";
+      const scope: string = "streaming playlist-read-private playlist-read-collaborative";
+      const redirect_uri: string = `${window.location.origin}/auth_callback`;
+      const urlBase: string = "https://accounts.spotify.com/authorize?";
       const params = {
-        response_type: "code",
-        client_id: client_id,
-        scope: scope,
-        redirect_uri: redirect_uri,
+        response_type: "code", client_id: client_id, scope: scope, redirect_uri: redirect_uri,
       };
-      const urlParams = new URLSearchParams(params).toString();
+      const urlParams: string = new URLSearchParams(params).toString();
       // Perform redirection to Spotify's authorization endpoint
-      //console.log(urlBase + urlParams);
       window.location.href = urlBase + urlParams;
-
-      
-      // // Get the returned user and update a new object.
-      // const user = new User(response.data);
-
-      // // Store the token into the local storage.
-      // localStorage.setItem("token", user.token);
-
-      // // Login successfully worked --> navigate to the route /game in the GameRouter
-      // navigate("/game");
     } catch (error) {
-      alert(
-        `Something went wrong during the login`
-      );
+      alert(`Something went wrong during the login`);
     }
   };
 
-  return ( <BaseContainer>
-      <div className="login container">
-        <div className="login form">
-          <div className="login button-container">
-            <Button
-              onClick={() => doLogin()}
-            >
-              Login with Spotify
-            </Button>
-          </div>
+  return (<BaseContainer>
+    <div className="login container">
+      <div className="login form">
+        <div className="login button-container">
+          <Button
+            onClick={() => doLogin()}
+          >
+            Login with Spotify
+          </Button>
         </div>
       </div>
-    </BaseContainer>
-  );
+    </div>
+  </BaseContainer>);
 };
 
 /**
