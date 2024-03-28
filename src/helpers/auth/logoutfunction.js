b6a63c18ad816618ea458b0cd7f957c56aef59bc
Niklas Schmidt <niklas.schmidt@uzh.ch>
2024-03-28 20:37:31 +0100

    added open todo in logout api call

--- src/helpers/auth/logoutfunction.js
+++ src/helpers/auth/logoutfunction.js
@@ -1,7 +1,8 @@
 import { api } from "../api";
 
 export const logout = async (navigate) => {
- 
+
+  //TODO: add header once clear how we do it
   const response = await api.delete("/auth/token")
   localStorage.clear()
   console.log("User logged out");
