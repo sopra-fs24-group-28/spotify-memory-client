a0a8bb5bd74ed8b5821d1aacf41377cc366212c9
niklasschm1dt <niklas.schmidt@uzh.ch>
2024-04-13 11:11:44 +0200

    fixing errors related to routing on the cloud, fixing redirect url in the frontend

--- src/components/views/Login.tsx
+++ src/components/views/Login.tsx
@@ -9,7 +9,7 @@ const Login = () => {
     try {
       const client_id: string = "5aac3ff5093942be92372c19a12fdecd";
       const scope: string = "streaming playlist-read-private playlist-read-collaborative user-modify-playback-state user-read-currently-playing playlist-read-private user-read-private";
-      const redirect_uri: string = `${window.location.origin}/auth_callback/`;
+      const redirect_uri: string = `${window.location.origin}/auth_callback`;
       console.log(redirect_uri);
       const urlBase: string = "https://accounts.spotify.com/authorize?";
       const params = {
