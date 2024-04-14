bea8bf898ed7981a692b9371316b19eafa5986a1
Diyar Taskiran <diyar.taskiran@uzh.ch>
2024-04-14 18:56:10 +0200

    updated lobbyobject and profile circle to correctly consume dtos

--- src/components/ui/LobbyObject.tsx
+++ src/components/ui/LobbyObject.tsx
@@ -2,16 +2,16 @@ import React, { useState } from "react";
 import "styles/ui/LobbyObject.scss";
 import { useNavigate } from "react-router-dom";
 import ProfileCircle from "./ProfileCircle";
+import PropTypes from "prop-types";
+import Lobby from "models/Lobby";
 import { Button } from "./Button";
 
 
-const LobbyObject = (props) => {
+const LobbyObject = (lobby: Lobby) => {
   const navigate = useNavigate();
-  const [playlistImg, setPlaylistImg] = useState("");
-  const [avatarImg, setAvatarImg] = useState("img");
-
+  const [playlistImg, setPlaylistImg] = useState(lobby.props.gameParameters.playlistImageUrl);
   function join() {
-    navigate("/Game1");
+    navigate("/Game" + lobby.props.lobbyId);
   }
 
   return (
@@ -19,16 +19,26 @@ const LobbyObject = (props) => {
       <div className="playlist-cover first-column-item">
         <img src={playlistImg} alt="RapCaviar" className="playlist-image" />
       </div>
-      <span className="playlist-name second-column-top-item">Rap</span>
+      <span className="playlist-name second-column-top-item">{lobby.props.gameParameters.playlist}</span>
       <div className="user-avatars second-column-bottom-item">
-        <ProfileCircle height={25} width={25} url={avatarImg} />
-        <ProfileCircle height={25} width={25} url={avatarImg} />
-        <ProfileCircle height={25} width={25} url={avatarImg} /><ProfileCircle height={25} width={25} url={avatarImg} />
+        {lobby.props.users.map((user) => (
+          <li key={user.userId}>
+            <ProfileCircle height={40} width={40} url={user.profileImageUrl}/>
+          </li>
+        ))}
       </div>
       <Button width="70%" height="50%" className="lobby-overview-button third-column-item" onClick={join}>Join</Button>
     </div>);
 
 };
+
+// Define prop-types for validation
+LobbyObject.propTypes = {
+  gameParameters: PropTypes.shape({
+    playlistImageUrl: PropTypes.string,
+  }).isRequired,
+};
+
 
 
 export default LobbyObject;
--- src/components/ui/ProfileCircle.tsx
+++ src/components/ui/ProfileCircle.tsx
@@ -11,9 +11,10 @@ const ProfileCircle = (props) => {
     height: props.height !== null ? props.height : 200,
     width: props.width !== null ? props.width : 200,
     background: props.url ? `url(${props.url}) center/cover` : "black",
-    border: "3px solid #FFFFFF",
-    filter: "drop-shadow(0px 10px 10px rgba(0, 0, 0, 0.25))",
+    border: "1px solid #D9D9D9",
+    //filter: "drop-shadow(0px 10px 10px rgba(0, 0, 0, 0.25))",
     borderRadius: "50%",
+    marginRight: "3px" // Adjust margin to allow overlap
   };
 
   return (
