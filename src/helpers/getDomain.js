dca75bfa6c37231051f04dd942e5fa33c18db930
Diyar Taskiran <diyar.taskiran@uzh.ch>
2024-04-18 17:58:39 +0200

    switched from ws to wss for secure websockets

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -14,8 +14,8 @@ export const getDomain = () => {
 };
 
 export const getWSDomain = () => {
-  const produUrl = "ws://sopra-fs24-group-28-server.oa.r.appspot.com/ws"; 
-  const devUrl = "ws://localhost:8080/ws";
+  const produUrl = "wss://sopra-fs24-group-28-server.oa.r.appspot.com/ws"; 
+  const devUrl = "wss://localhost:8080/ws";
 
   return isProduction() ? produUrl : devUrl;
 }
