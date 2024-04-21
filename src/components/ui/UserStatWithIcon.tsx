59a1c7f2437ec46f9dfdadfc0691efd8a39a2750
Diyar Taskiran <diyar.taskiran@uzh.ch>
2024-04-21 17:33:32 +0200

    established inital ws connection in lobby waiting room

--- src/components/ui/UserStatWithIcon.tsx
+++ src/components/ui/UserStatWithIcon.tsx
@@ -3,16 +3,26 @@ import PropTypes from "prop-types";
 import "../../styles/ui/UserStatWithIcon.scss";
 import ProfileCircle from "./ProfileCircle";
 
-export const UserStatWithIcon = (props) => (<span className="userbox">
+export const UserStatWithIcon = (props) => {
+  const user = props.user;
+  
+  return (
+  <span className="userbox">
   <span className="centered-column">
-    <ProfileCircle width={"30px"} height={"30px"}></ProfileCircle>
+    <ProfileCircle width={"40px"} height={"40px"} url={user.profileImageUrl}></ProfileCircle>
   </span>
 
-  <span className="button-label">{props.username}</span>
-  <span className="notification-bubble">{props.currentStanding}</span>
+  <span className="button-label">{user.username}</span>
+  {props.currentStanding > 0 && ( // Check if currentStanding is larger than 0
+        <span className="notification-bubble">{props.currentStanding}</span>
+      )}
   </span>);
 
+  }
 
 UserStatWithIcon.propTypes = {
-  username: PropTypes.string, currentStanding: PropTypes.string,
+    user: PropTypes.object,
+    username: PropTypes.string,
+    currentStanding: PropTypes.number
 };
+  
--- src/components/views/LobbyWaitingRoom.tsx
+++ src/components/views/LobbyWaitingRoom.tsx
@@ -6,39 +6,46 @@ import { UserStatWithIcon } from "../ui/UserStatWithIcon";
 import PropTypes from "prop-types";
 import { api, handleError } from "helpers/api";
 import SpotifyLogoWithTextSVG from "../ui/icons-svg/SpotifyLogoWithTextSVG";
-import wsHandler from "../../helpers/wsHandler.js";
-import Game from "./Game";
+import WSHandler from "../../helpers/wsHandler.js";
+import Game from "models/Game";
+import Lobby from "models/Lobby";
 
 
-const LobbyWaitingRoom = () => {
+const LobbyWaitingRoom = (state) => {
   const navigate:NavigateFunction = useNavigate();
-  const ws:wsHandler = null;
   const location = useLocation();
   const initialGameId = location.state.lobby.lobbyId;
   const [game, setGame] = useState();
-
-
+  
+  
   //Websocket specific
 
   const receiverFunction = (newDataRaw) => {
-    const parsedData = JSON.parse(newDataRaw.body).gameChangesDTO;
-    const gameData = parsedData.game;
+    console.log("WS received new data: ", newDataRaw);
+    // const parsedData = JSON.parse(newDataRaw.body).gameChangesDTO;
+    // const gameData = parsedData.game;
 
-    if (gameData.changed) {
-      setGame(prev => {
-        return Game(gameData.value);
-      });
-    }
+    // if (gameData.changed) {
+    //   setGame(prev => {
+    //     return Game(gameData.value);
+    //   });
+    // }
   };
+  const ws = new WSHandler(`/games/${initialGameId}`, 
+                          `/queue/games/${initialGameId}`, 
+                          `app/games/${initialGameId}`, 
+                          receiverFunction);
 
   async function fetchData() {
     try {
       console.log(initialGameId);
       const response = await api.get(`/games/${initialGameId}`);
       const gameStart = response.data;
-      setGame(Game(gameStart));
-
-      return game;
+      console.log(gameStart);
+      
+      // instantiating a lobby object here instead of a game object
+      // as the ws returns data appropriate for this class. But object is later cast into game when appropriate
+      return new Lobby(initialGameId, gameStart); 
 
     } catch (error) {
       console.error(`Something went wrong while fetching the Game: \n${handleError(error)}`);
@@ -47,23 +54,22 @@ const LobbyWaitingRoom = () => {
 
   useEffect(() => {
     const fetchDataAndConnect = async () => {
-      await fetchData();
-      console.log("fetchDataAndConnect called");
-      console.log(game);
-      const ws = wsHandler(`/games/${game.gameId}`, `/queue/game/${game.gameId}`, `app/game/${game.gameId}`, receiverFunction);
+      const initGame = await fetchData();
+      setGame(initGame);
       await ws.connect();
+
     };
-    fetchDataAndConnect().catch(error => {alert("Something went wrong in the initialisation of the individual lobby. Please consult the admin")});
-
+    fetchDataAndConnect();
+    
   }, []);
-
-  useEffect(() => {
-    console.log("game changed");
-    console.log(game);
-    if (game?.gameState === "ONPLAY"){
-      navigate(`game/${game.gameId}`, { state: {ws: ws, gameId: game.gameId } })
-    }
-  }, [game]);
+  
+  // useEffect(() => {
+  //   console.log("game changed");
+  //   console.log(game);
+  //   if (game?.gameState === "ONPLAY"){
+  //     navigate(`game/${game.gameId}`, { state: {ws: ws, gameId: game.gameId } })
+  //   }
+  // }, [game]);
 
 
   //ComponentSpecific
@@ -90,18 +96,22 @@ const LobbyWaitingRoom = () => {
     //TODO: send ready state via websocket connection:
   }
 
+  function handleStart() {
+    // TODO: send start signal via websocket
+  }
+
   return (<div className="BaseContainer">
     <div className="BaseDivLobby">
       <div className="gridhandler">
         <div className="centerwrapper">
           <div
-            className={game?.gameParameters?.playlist?.images?.[0] ? "imgContainer" : "spotifyPlaylistContainer"}>
-            {game?.gameParameters?.playlist?.images?.[0] ? (
+            className={game?.gameParameters.playlist.playlistImageUrl ? "imgContainer" : "spotifyPlaylistContainer"}>
+            {game?.gameParameters.playlist.playlistImageUrl? (
               <img
-                src={game.gameParameters.playlist.images[0]}
+                src={game?.gameParameters.playlist.playlistImageUrl}
                 alt="Spotify Playlist Image"
-                width="85%"
-                height="85%"
+                width="100%"
+                height="100%"
                 className="img"
               />
             ) : (
@@ -112,16 +122,24 @@ const LobbyWaitingRoom = () => {
         <div className="centerwrapper">
           <div className="playergrid">
             <div className="h3-title">These players are already waiting!!</div>
-            {game && game.players && game.players.map((player, index) => (
-              <UserStatWithIcon key={index} username={player.username} currentStanding="?"></UserStatWithIcon>
+            {game && game.playerList && game.playerList.map((user) => (
+              <li key={user.userId} className="grid-item">
+                <UserStatWithIcon user={user} currentStanding={0}></UserStatWithIcon>
+              </li>
             ))}
           </div>
         </div>
 
         <div className="centerwrapper">
           <div className="buttonContainer">
-            <Button width="45%" onClick={handleLeave}>Leave</Button>
-            <Button width="45%" onClick={handleLeave}>I am ready</Button>
+            <Button width="65%" onClick={handleLeave}>Leave</Button>
+            {/* <Button width="45%" onClick={handleLeave}>I am ready</Button> 
+                TODO: for how, I am ready is not shown, reinsert if implemented*/} 
+            
+            {/* todo: only show this if player is host  */}
+            <Button width="65%" onClick={handleStart}>Start</Button> 
+          </div>
+          <div className="buttonContainer">
           </div>
         </div>
       </div>
