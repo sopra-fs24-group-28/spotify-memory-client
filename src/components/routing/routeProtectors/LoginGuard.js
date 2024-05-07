723f31616300e43d27d813a706cd29dd44bf5f58
niklasschm1dt <niklas.schmidt@uzh.ch>
2024-05-07 16:46:33 +0200

    implemented working guards

--- src/components/routing/routeProtectors/LoginGuard.js
+++ src/components/routing/routeProtectors/LoginGuard.js
@@ -2,13 +2,6 @@ import React from "react";
 import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { logout } from "../../../helpers/auth/logoutfunction";
-import toastNotify from "../../../helpers/Toast";
-
-/**
- *
- * Another way to export directly your functional component is to write 'export const'
- * instead of 'export default' at the end of the file.
- */
 export const LoginGuard = () => {
   const navigate = useNavigate();
   if (!localStorage.getItem("token")) {
